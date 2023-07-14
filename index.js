aafcb311cc7b30ce6925d40753c0e88e10c7c54c
gevyndo <gevyndo18@gmail.com>
2023-07-14 14:51:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import ProductRoute from "./routes/ProductRoute.js";
-import UserRouter from "./routes/UserRoute.js";
+import UserRoute from "./routes/UserRoute.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(ProductRoute);
+app.use('/api/products', ProductRoute); 
+app.use('/api/users', UserRoute); 
 
-
-app.listen(process.env.APP_PORT, ()=>{
-    console.log("Server running at port 5000");
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server running at port 5000");
 });
